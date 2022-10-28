65dbab797dabdf1eeabe64bfba9577a5d2eec394
priyanka-gite <pgite7@gmail.com>
2022-10-28 21:01:52 +0200

    opdracht-one-with-new-methods

---
+++ opdracht-two.js
@@ -0,0 +1,14 @@
+// Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+const totalSale = (arr) => {
+    let total = 0;
+    for (let i = 0 ; i < arr.length ; i++){
+        total += arr[i].sold;
+        total++;
+    }
+    return total
+}
+console.log("we sold "+ totalSale(inventory)+ " number of tv.");
+
+//Opdracht 2b: Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
+
+function changeColor
