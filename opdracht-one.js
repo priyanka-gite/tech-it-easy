62009d4b7b4d9b58dcc53103a9f9a7819863a446
priyanka-gite <pgite7@gmail.com>
2022-10-28 17:30:00 +0200

    opdracht-een-done

--- opdracht-one.js
+++ opdracht-one.js
@@ -22,3 +22,20 @@ const soldOutTv = (arr) => {
         return newSoldOut;
 }
     console.log (soldOutTv(inventory));
+
+//Opdracht 1c: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight beschikken. Log de uitkomst in de console.
+
+const tvWithAmbilights = (arr) => {
+    const hasAmbilights = [];
+    for(let i = 0 ; i < arr.length ; i++){
+        if (arr[i].options.ambiLight=== true){
+            hasAmbilights.push(arr[i])
+        }
+    }
+    return hasAmbilights;
+}
+console.log(tvWithAmbilights(inventory));
+
+//Opdracht 1d: Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert. Log de uitkomst in de console.
+const num = inventory .sort((a,b) =>(a.price > b.price ? 1 : -1));
+console.log(num);
