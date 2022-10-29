306915bd39fcf7d7a9d05bcc4626e3fa854b9bed
priyanka-gite <pgite7@gmail.com>
2022-10-29 21:04:07 +0200

    opdracht-two-incomplete

--- opdracht-two.js
+++ opdracht-two.js
@@ -1,4 +1,5 @@
 // Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+
 const totalSale = (arr) => {
     let total = 0;
     for (let i = 0 ; i < arr.length ; i++){
@@ -7,8 +8,12 @@ const totalSale = (arr) => {
     }
     return total
 }
-console.log("we sold "+ totalSale(inventory)+ " number of tv.");
+console.log(`we sold  ${totalSale(inventory)} number of tv.`);
+
 
 //Opdracht 2b: Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
 
-function changeColor
+const totSale = document.getElementById("totSale")
+totSale.textContent = totalSale(inventory);
+
+//Opdracht 2d: Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
