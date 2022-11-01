c8b85dfc9f3936c074b720d6be5e48e254e63336
priyanka-gite <pgite7@gmail.com>
2022-11-01 13:49:22 +0100

    opdracht-three-changed

--- opdracht-three.js
+++ opdracht-three.js
@@ -1,9 +1,26 @@
 // Opdracht 3a: Gebruik een array-methode om alle tv merken (zoals Philips, NIKKEI, etc.) in een lijst op de pagina weer te geven. Zorg ervoor dat dit ook zou werken als we 200 tv's in onze array zouden hebben staan. Dat er dubbele namen in zitten, is niet erg.
 
-const brands = inventory.map((tvBrand) => {
-   return tvBrand.brand
+const brands = inventory.map((brand) => {
+return brand.brand
 })
-console.log(brands);
+console.log(`brands are ${brands}`);
 
-const tvBrands = document.getElementById("brands");
-tvBrands.textContent = brands;
+let list = document.getElementById("brands")
+
+brands.forEach((item) => {
+   let li = document.createElement("li");
+   li.innerText = item;
+   list.appendChild(li);
+})
+
+//Opdracht 3b: Schrijf de code uit 3a om naar een functie die een array met tv-objecten verwacht. Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken. Tip: vergeet deze functie -declaratie niet aan te roepen!
+
+const listOfBrands = (arr) => {
+   for (let i = 0 ; i< arr.length; i++) {
+     if (arr[i].brand == true) {
+        return arr[i].brands;
+     }
+   }
+
+}
+console.log(`brands are ${listOfBrands(inventory)}`);
