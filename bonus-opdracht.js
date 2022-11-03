40bd1fc76145edf26844272de8ad011ce7750d4b
priyanka-gite <pgite7@gmail.com>
2022-11-03 21:20:45 +0100

    final

--- bonus-opdracht.js
+++ bonus-opdracht.js
@@ -1,5 +1,15 @@
+
 // Maak drie knoppen op de pagina: Sorteer op prijs, AmbiLight TV's en Uitverkochte exemplaren. Gebruik de code die je in opdracht 1b, 1c en 1d hebt gemaakt en schrijf dit om naar functies zodat je ze kunt aanroepen op het moment dat de buttons geklikt worden. Zorg ervoor dat de functies de uitkomsten in de de console loggen als de gebruiker op de bijbehorende knop klikt. Tip: lees hiervoor paragraaf 7.4 op EdHub eens door!
-
-
+function sorted () {
+     console.log(num(inventory))
+}
+function ambilights (){
+     console.log(tvWithAmbilight)
+}
+function soldOutTelevision () {
+     console.log(soldOutTv)
+}
 
 // Zorg er nu voor, in plaats van dat de uitkomsten in de console worden gelogd, dat de uitkomsten worden meegegeven aan jouw functie uit 4e zodat de resultaten daadwerkelijk op de pagina weergegeven worden!
+// const sortedTelevisionPrice = document.getElementById("sorting");
+// sortedTelevisionPrice.innerHTML = ;
--- opdracht-four.js
+++ opdracht-four.js
@@ -9,8 +9,8 @@
 
 // a) create a function
 
-const tvDetails = name =>
-    `${name.brand} ${name.type} - ${name.name}` ;
+const tvDetails = tv =>
+    `${tv.brand} ${tv.type} - ${tv.name}` ;
 
 // b)call the function we made
 const oneTv = inventory.map(tvDetails);
@@ -19,41 +19,49 @@ console.log(oneTv[1]);
 
 
 // Opdracht 4b: Maak een herbruikbare functie die de prijs van één tv als parameter verwacht (zoals 379 of 159) teruggeeft in het format €379,- of €159,-.
-const priceOFTv = (price) =>{
-    return  ` €${price},- `
+const priceOFTv = (tv) =>{
+    return  ` €${tv.price},- `
 }
-
-console.log(priceOFTv(356));
+const prices = inventory.map(priceOFTv);
+console.log(prices[1]);
 
 //
 // Opdracht 4c: Maak een herbruikbare functie die een string genereert voor alle beschikbare schermgroottes van één tv. De functie geeft dit terug in het format [schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm) etc. Als een tv maar één schermgrootte heeft ([32]) wordt de output 32 inch (81 cm). Wanneer een tv vier schermgroottes heeft ([43, 50, 55, 58]) wordt de output 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm). Let op: om één string te genereren uit een array van schermgroottes zul je een for-loop voor moeten gebruiken.
 //
-const screenSize = (arr) => {
-    let total = ''; //total is initialied to zero
-    for (let i = 0 ; i < arr.length ; i++) {
-         total += `${arr[i]} inch ${arr[i] * 2.54} cm |`
-//        initial value of total is empty;
-//on first iteration value of total is 43 inch 109.22 cm;
-//       on second iteration value of total is 50 inch 127 cm
-//        on third iteration value of total is 55 inch 139.7 cm
-//        on fourth iteration value of total is 58 inch 147.32 cm
-//        iteration stopped
+const screenSize = (tv) => {
+    let result = ''; //reult is initialied to zero
+    for (let i = 0 ; i < tv.availableSizes.length ; i++) {
+        result += `${tv.availableSizes[i]} inch ${tv.availableSizes[i] * 2.54} cm |`;
     }
     // out of loop and return total;
-    return total;
+    return result;
    }
-console.log(screenSize([43, 50, 55, 58]))
-
+   const sizes= inventory.map(screenSize);
+console.log(sizes[1])
 // Opdracht 4d: Schrijf een script die de informatie van de Philips 43PUS6504/12 tv weergeeft op de pagina zoals onderstaand voorbeeld. Gebruik de functies die je hebt gemaakt in opdracht 4a, 4b en 4c.
 //
 // Philips 43PUS6504/12 - 4K TV
 // €379,-
 // 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm)
 const tvDet = document.getElementById("tvDetails");
-tvDet.innerText = oneTv[1];
+tvDet.innerText = oneTv[2];
 
 const price = document.getElementById("priceOfTv");
-price.innerText = priceOFTv(365);
+price.innerText = prices[1];
 
 const screen = document.getElementById("screenSize");
-screen.innerHTML = screenSize([43, 50, 55, 58]);
+screen.innerHTML = sizes[4];
+
+//Opdracht 4e: Maak een herbruikbare functie die de informatie van alle tv's weergeeft op de pagina. Gebruik hiervoor de map-methode in combinatie met de functies die je hebt gemaakt in opdracht 4a, 4b en 4c.
+
+// function allTelevision(inputArr) {
+//     const listOfTelevision = document.getElementById("allDetails")
+//     inputArr.map((oneTv)=>{
+//   listOfTelevision.innerHTML +=
+//       <h2 id="tvDetails" className="color-Tv-Details"> ${tvDetails(oneTv)} </h2>
+//         <h2 id="priceOfTv" className="color-price"> ${priceOFTv(oneTv)} </h2>
+//         <h2 id="screenSize" className="color-screensize">   ${screenSize(oneTv)} </h2>
+//
+//     })
+// }
+// console.log(allTelevision(inventory));
